3f8e2ea72277b620446527e7c17a33f75339d6c8
mfrtest2 <122350036+mfrtest2@users.noreply.github.com>
2023-11-10 12:28:52 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,2 +1,14 @@
-# write your code here
-# 
+def create_report(data_file_name: str, report_file_name: str) -> None:
+    with open(data_file_name, "r") as f:
+        supply = 0
+        buy = 0
+        for line in f:
+            lst = line.split(",")
+            if lst[0] == "buy":
+                buy += int(lst[1])
+            else:
+                supply += int(lst[1])
+    with open(report_file_name, "w") as f:
+        f.write(f"supply,{supply}\n")
+        f.write(f"buy,{buy}\n")
+        f.write(f"result,{supply - buy}\n")
